47362478d46bc71dac3b22c60ff814e86936f316
github username <github email address>
2020-03-17 10:26:53 -0400

    first commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,27 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    Author.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    articles.map do |article|
+      article.magazine
+    end
   end
 
 
