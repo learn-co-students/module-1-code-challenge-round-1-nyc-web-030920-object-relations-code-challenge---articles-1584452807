85df70bf96ebf1c936bf3b7363479485c9ed88ec
Vivica <Ma>
2020-03-17 10:50:09 -0400

    second commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,26 @@
 class Article
+    attr_reader :author, :magazine, :title
+    @@all = []
+     
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        Article.all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def author 
+        Magazine.all.select do |magazine|
+            magazine.article == self
+        end 
+    end 
+
 
 
 end
+
+
