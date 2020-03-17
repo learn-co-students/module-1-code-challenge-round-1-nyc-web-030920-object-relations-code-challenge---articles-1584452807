a46b2b3a37cfeb91f52a6d0a87324fbbf78e64da
Yueying <wangyy395@gmail.com>
2020-03-17 10:46:28 -0400

    first commit

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -29,6 +29,25 @@ class Magazine
     end.uniq
   end
 
+  def self.find_by_name(name)
+    Magazine.all.find do |magazine_instance|
+      magazine_instance.name == name 
+    end
+  end
 
+  #Returns an array strings of the titles of all articles written for that magazine
+  def article_titles
+    self.articles.map do |article_instance|
+      article_instance.title 
+    end
+  end
+
+  #Returns an array of authors who have written more than 2 articles for the magazine
+  def contributing_authors
+    self.contributors.select do |author_instance|
+      #might return articles for aother magazine?
+      author_instance.articles > 2 
+    end
+  end
 
 end
