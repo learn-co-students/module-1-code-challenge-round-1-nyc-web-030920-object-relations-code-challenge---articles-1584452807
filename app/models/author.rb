169568eca38004d7cac104f801154932c8a24934
jnzhang622 <jnzhang622@gmail.com>
2020-03-17 10:40:45 -0400

    message

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,17 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
-  
   end
 
+  def articles #Returns an array of Article instances the author has written
+    Article.all.select {|a| a.author == self}
+  end
+
+  def magazines #Returns a unique array of Magazine instances for which the author has contributed to
+    articles.map {|a| a.magazine}
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,22 @@
 class Magazine
   attr_accessor :name, :category
-
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    Magazine.all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def article_list
+    Article.all.select {|a| a.magazine == self}
+  end
+
+  def contributors
+    article_list.map {|a| a.author}
 
 end
