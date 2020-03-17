48b4b10094d59a31fc18c85c445582e5e28af336
duruchima <duruchima1@gmail.com>
2020-03-17 10:40:46 -0400

    saving changes

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,30 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
   
+  end
+
+  def articles
+    Article.all.select {|articles| articles.author == self}
+  end
+
+  def magazines
+    unique_array = []
+    unique_array << articles.map {|articles| articles.magazines}
+    unique_array.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    unique_array = []
+    unique_array << articles.map {|articles| articles.magazine.category}
+    unique_array.uniq
   end
 
 
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,27 @@
 class Magazine
   attr_accessor :name, :category
-
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    Magazine.all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def all_articles
+    Article.all.select {|articles| articles.magazine == self}
+  end
+
+  def contributors
+    all_articles.map {|articles| articles.author}
+  end
+
+  def self.find_by_name(name)
+    Magazine.all.find {|magazine| magazine.name == name}
   end
 
 
