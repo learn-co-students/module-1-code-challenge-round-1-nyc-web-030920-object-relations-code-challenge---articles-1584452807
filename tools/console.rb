a068defe15ef2e01c109a5f3d7dc81c6178ee329
github username <github email address>
2020-03-17 11:01:31 -0400

    saving progress

--- tools/console.rb
+++ tools/console.rb
@@ -11,6 +11,14 @@ end
 tolkien = Author.new("J.R.R Tolkien")
 
 fantasy = Magazine.new("Fantasy Fans Weekly", "Fantasy")
+sports = Magazine.new("Runners World", "Sports")
+parenthood = Magazine.new("NYC Parenting", "Parenthood")
+
+tolkien.add_article(fantasy, "My Hobbit")
+tolkien.add_article(sports, "Running in Middle Earth")
+tolkien.add_article(parenthood, "Bringing up Bulrog")
+
+
 
 # tolkien.add_article(fantasy, "My Homely Hobbit")
 
