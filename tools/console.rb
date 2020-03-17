e1b93655cd7c5aa6f0a3cca92c592ff6694d1bdd
Yueying <wangyy395@gmail.com>
2020-03-17 10:49:44 -0400

    second commit

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,18 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+a1 = Author.new('wyy')
+a2 = Author.new('dyl')
+a3 = Author.new('hy')
+
+m1 = Magazine.new('bunny', 'pet')
+m1 = Magazine.new('bunny', 'pet')
+m1 = Magazine.new('bunny', 'pet')
+
+art1 = Article
+
+
+
 
 
 
